556356099600b08d09a00dc779deb3965bb56548
Hozzy224 <32251523+Hozzy224@users.noreply.github.com>
2017-09-30 00:52:04 -0400

    take three
    .

---
+++ Hozzy's Hideout_Upload/Message center(var).php
@@ -0,0 +1,5 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWESendMail = '{"WEca420555e0":{"ScriptVersion":210,"ConfSendMail":{"IsUpdated":true,"Type":3,"TypeStr":"SMTP","SpecialType":"","SMTPHost":null,"SMTPPort":25,"SMTPUser":null,"SMTPPassWrd":null,"SMTPSSL":false},"FormLinks":{"ElementsID":{"WE8c6374ee21":"WETextArea2","WE0635d8ee50":"WEListBox2"},"ButtonSubmitID":"WE54c6136522","ButtonCancelID":null},"SendMailInfo":{"AdminEmail":{"Items":{"DEFAULT":"hozzy.payment@gmail.com"}},"CopycacheEmail":{"Items":{"DEFAULT":""}},"CopyEmail":{"Items":{"DEFAULT":""}},"ReturnEmail":"web.hz.mail@gmail.com","ReturnEmailID":null,"Subject":"User email","SendReceipt":false,"ReceiptHeader":{"Items":{"DEFAULT":""}},"ReceiptBody":{"Items":{"DEFAULT":""}},"SendReceiptSignature":false,"ReceiptSignature":{"Target":null,"Links":{"Items":{"DEFAULT":""}}},"UserEmailID":null,"ListInputTitle":{"WE8c6374ee21":{"Items":{"DEFAULT":"Body"}},"WE0635d8ee50":{"Items":{"DEFAULT":"Tag"}}}}}}';
+$OEConfWETextArea = '{"WE8c6374ee21":{"InputName":"WE8c6374ee21"}}';
+$OEConfWEListBox = '{"WE0635d8ee50":{"InputName":"WE0635d8ee50"}}';
